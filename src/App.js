f1955dba59cf067db8a559109c626af2e1881a8d
Jonathan Delgado <jonathan.degado6@gmail.com>
2024-09-18 17:16:24 -0600

    useFormik

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { Formik, Form, Field, ErrorMessage } from "formik"
+import TextInput from './components/TextInput'
+
+const validate = (values) => {
+  const errors = {}
+  if(!values.name){
+    errors.name = 'Requerido'
+  }else if(values.name.length < 5 ){
+    errors.name = 'El nombre es muy corto'
+  }
+ 
+  if(!values.lastname){
+    errors.lastname = 'Requerido'
+  }else if(values.lastname.length < 5 ){
+    errors.lastname = 'El Apellido es muy corto'
+  }
+
+  return errors
+}
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Formik
+        initialValues={{name :'', lastname :'', email :'', }}
+        validate={validate}
+        onSubmit= {values => console.log(values)}
+    >
+      <Form>
+        <TextInput name="name" label="Nombre"/>
+        {/* <Field name='select' as="select" className='input'>
+          <option value="chanchitofeliz">Chanchito Feliz</option>
+          <option value="chanchitofeli">Chanchito Feliz</option>
+          </Field> */}
+        <br/>
+        <label>Apellido</label>
+        <Field name='lastname' type='text'/>
+        <ErrorMessage name="lastname"/>
+        <br/>
+        <label>Email</label>
+        <Field name='email' type='email'/>
+        <ErrorMessage name="email"/>
+        <button type="submit">Enviar</button>
+      </Form>
+    </Formik>
   );
 }
 
